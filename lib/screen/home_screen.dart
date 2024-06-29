8345eab245a2d581bb3cc149c79eed9ac4f38cb3
HiloongHo <hiloongho@gmail.com>
2024-06-29 20:35:08 +0800

    实现了获取地理位置，时间显示

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controller/global_controller.dart';
+import '../widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,11 +12,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final GlobalController globalController =
-      Get.put(GlobalController(), permanent: true);
+  // 使用GetX库创建一个全局且永久的GlobalController实例
+  final GlobalController globalController = Get.put(
+      // 实例化GlobalController
+      GlobalController(),
+      // 设置permanent为true，确保该Controller在整个应用生命周期内保持活跃，
+      // 即使页面切换，也能保持状态，适合管理应用级别的状态信息
+      permanent: true);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Obx(() => globalController.checkLoading().isTrue
+              ? const Center(child: CircularProgressIndicator())
+              : ListView(
+            scrollDirection: Axis.vertical,
+            children: const[
+               SizedBox(height: 20),
+               HeaderWidget(),
+            ],
+          ))),
+    );
   }
 }
---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:get/get.dart';
+
+import '../controller/global_controller.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({super.key});
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  String city = "";
+  String date =
+      "${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}";
+
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
+
+  @override
+  void initState() {
+    getAddress(globalController.getLattitude().value,
+        globalController.getLongitude().value);
+    // TODO: implement initState
+    super.initState();
+  }
+
+  getAddress(lat, lon) async {
+    List<Placemark> placemark = await placemarkFromCoordinates(lat, lon);
+    print(placemark);
+    Placemark place = placemark[0];
+    setState(() {
+      city = place.locality!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            city,
+            style: const TextStyle(fontSize: 35, height: 2),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            date,
+            style:
+                TextStyle(fontSize: 14, color: Colors.grey[700], height: 1.5),
+          ),
+        ),
+      ],
+    );
+  }
+}
